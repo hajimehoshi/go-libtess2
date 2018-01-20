fad5f57e4b35f062d3363707ebe0f0aa74630d7d
Hajime Hoshi <hajimehoshi@gmail.com>
2018-01-21 01:33:05 +0900

    tess: Port tessMeshTessellateMonoRegion to Go

--- tess.go
+++ tess.go
@@ -27,6 +27,7 @@
 package libtess2
 
 // #include "tess.h"
+// #include "geom.h"
 //
 // #include <stdlib.h>
 // #include <string.h>
@@ -80,7 +81,6 @@ package libtess2
 // }
 //
 // void tessProjectPolygon( TESStesselator *tess );
-// int tessMeshTessellateMonoRegion( TESSmesh *mesh, TESSface *face );
 import "C"
 
 import (
@@ -152,6 +152,77 @@ func (t *Tesselator) Tesselate() ([]int, []Vertex, error) {
 	return elements, vertices, nil
 }
 
+// tessMeshTessellateMonoRegion tessellates a monotone region
+// (what else would it do??)  The region must consist of a single
+// loop of half-edges (see mesh.h) oriented CCW.  "Monotone" in this
+// case means that any vertical line intersects the interior of the
+// region in a single interval.
+//
+// Tessellation consists of adding interior edges (actually pairs of
+// half-edges), to split the region into non-overlapping triangles.
+//
+// The basic idea is explained in Preparata and Shamos (which I don''t
+// have handy right now), although their implementation is more
+// complicated than this one.  The are two edge chains, an upper chain
+// and a lower chain.  We process all vertices from both chains in order,
+// from right to left.
+//
+// The algorithm ensures that the following invariant holds after each
+// vertex is processed: the untessellated region consists of two
+// chains, where one chain (say the upper) is a single edge, and
+// the other chain is concave.  The left vertex of the single edge
+// is always to the left of all vertices in the concave chain.
+//
+// Each step consists of adding the rightmost unprocessed vertex to one
+// of the two chains, and forming a fan of triangles from the rightmost
+// of two chain endpoints.  Determining whether we can add each triangle
+// to the fan is a simple orientation test.  By making the fan as large
+// as possible, we restore the invariant (check it yourself).
+func tessMeshTessellateMonoRegion(mesh *C.TESSmesh, face *C.TESSface) {
+	// All edges are oriented CCW around the boundary of the region.
+	// First, find the half-edge whose origin vertex is rightmost.
+	// Since the sweep goes from left to right, face.anEdge should
+	// be close to the edge we want.
+	up := face.anEdge
+	assert(up.Lnext != up && up.Lnext.Lnext != up)
+
+	for C.VertLeq(dst(up), up.Org) != 0 {
+		up = lPrev(up)
+	}
+	for C.VertLeq(up.Org, dst(up)) != 0 {
+		up = up.Lnext
+	}
+	lo := lPrev(up)
+
+	for up.Lnext != lo {
+		if C.VertLeq(dst(up), lo.Org) != 0 {
+			// up.Dst is on the left.  It is safe to form triangles from lo.Org.
+			// The EdgeGoesLeft test guarantees progress even when some triangles
+			// are CW, given that the upper and lower chains are truly monotone.
+			for lo.Lnext != up && (C.EdgeGoesLeft(lo.Lnext) != 0 || C.tesedgeSign(lo.Org, dst(lo), dst(lo.Lnext)) <= 0) {
+				tempHalfEdge := C.tessMeshConnect(mesh, lo.Lnext, lo)
+				lo = tempHalfEdge.Sym
+			}
+			lo = lPrev(lo)
+		} else {
+			// lo.Org is on the left.  We can make CCW triangles from up.Dst.
+			for lo.Lnext != up && (C.EdgeGoesRight(lPrev(up)) != 0 || C.tesedgeSign(dst(up), up.Org, lPrev(up).Org) >= 0) {
+				tempHalfEdge := C.tessMeshConnect(mesh, up, lPrev(up))
+				up = tempHalfEdge.Sym
+			}
+			up = up.Lnext
+		}
+	}
+
+	// Now lo.Org == up.Dst == the leftmost vertex.  The remaining region
+	// can be tessellated in a fan from this leftmost vertex.
+	assert(lo.Lnext != up)
+	for lo.Lnext.Lnext != up {
+		tempHalfEdge := C.tessMeshConnect(mesh, lo.Lnext, lo)
+		lo = tempHalfEdge.Sym
+	}
+}
+
 // tessMeshTessellateInterior tessellates each region of
 // the mesh which is marked "inside" the polygon.  Each such region
 // must be monotone.
@@ -161,7 +232,7 @@ func tessMeshTessellateInterior(mesh *C.TESSmesh) {
 		// Make sure we don't try to tessellate the new triangles.
 		next = f.next
 		if f.inside != 0 {
-			C.tessMeshTessellateMonoRegion(mesh, f)
+			tessMeshTessellateMonoRegion(mesh, f)
 		}
 	}
 }
